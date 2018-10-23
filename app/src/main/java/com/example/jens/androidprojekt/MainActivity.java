9546a6d397045b278ff3154beb07de8f75d4d876
jens <jenshabermann89@yahoo.com>
2018-10-23 16:15:28 +0200

    nichts getan

--- app/src/main/java/com/example/jens/androidprojekt/MainActivity.java
+++ app/src/main/java/com/example/jens/androidprojekt/MainActivity.java
@@ -2,9 +2,17 @@ package com.example.jens.androidprojekt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+import lejos.nxt.Motor;
 import lejos.nxt.comm.Bluetooth;
+import lejos.nxt.comm.BTConnection;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
 
 
@@ -12,5 +20,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+    public static void main(String [] args)
+    {
+        BTConnection btc = Bluetooth.waitForConnection();
+        DataInputStream dIN = btc.openDataInputStream();
+        DataOutputStream dOut = btc.openDataOutputStream();
+
+
+
     }
 }
