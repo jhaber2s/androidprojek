cb201e0b8f558890e04e4eb6a86a941a3e6bf926
jens <jenshabermann89@yahoo.com>
2018-10-30 13:41:36 +0100

    Bluetoothconnect kommentiert

--- app/src/main/java/com/example/jens/androidprojekt/BluetoothConnector.java
+++ app/src/main/java/com/example/jens/androidprojekt/BluetoothConnector.java
@@ -9,22 +9,26 @@ import java.io.IOException;
 import java.util.UUID;
 
 public class BluetoothConnector {
-
+    // Variabelen zum Conect
     public BluetoothAdapter bluetoothAdapter;
     public BluetoothSocket bluetoothSocket;
     public String address;
 
+    //Constructor mit Adress uebergabe
     public BluetoothConnector(String adress) {
         this.address = adress;
         this.bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     }
 
+    //Verbindungs Methode
     public boolean connect() {
         boolean connected = false;
 
+        //nxt als ziel waehlen
         BluetoothDevice nxt = this.bluetoothAdapter.getRemoteDevice(this.address);
 
 
+        //connect
         try {
             this.bluetoothSocket = nxt.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
             this.bluetoothSocket.connect();
